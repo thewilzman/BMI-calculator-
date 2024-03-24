d9b0e81c96df85b9629a503e28ddb5bc4601582f
thewilzman <124735016+thewilzman@users.noreply.github.com>
2024-03-24 15:42:11 -0400

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,13 @@
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+a = float(height)
+
+b= int(weight)
+ 
+bmi= b/(a ** 2)
+
+print("Your calculated BMI is " + int(bmi))
+                   
